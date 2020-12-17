0b7e88b9ff92f57716a86712320d849a14623dbd
Agne Varanauskiene <50881262+agnevar@users.noreply.github.com>
2020-12-17 16:05:33 +0200

    First exercise

---
+++ First/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert text: ");
+            var word = Console.ReadLine();
+            var firstLetter = word[0];
+
+            var newWord = firstLetter + word + firstLetter;
+
+            Console.WriteLine(newWord);
+
+        }
+    }
+}
