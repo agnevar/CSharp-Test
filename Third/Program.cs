353ff9b322ec4f40fce96a7edc7511a5a8e7869a
Agne Varanauskiene <50881262+agnevar@users.noreply.github.com>
2020-12-17 16:08:42 +0200

    Thrid exercise

---
+++ Third/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type your word: ");
+            var word = Console.ReadLine();
+
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+
+            Console.WriteLine("Word in reverse is: " + new string(charArray));
+      
+        }
+    }
+}
